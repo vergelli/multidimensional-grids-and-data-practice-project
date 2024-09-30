243ed52fe07989aac36a14e2af1442d7343f4c15
vergelli <federico_joelvergelli@hotmail.com>
2024-09-30 20:44:18 -0300

    feat(CUDA-imp): Se modifica comando de compilacion para windows.

--- compilation.bat
+++ compilation.bat
@@ -1,3 +1,3 @@
 @echo off
-nvcc .\src\main.cpp .\src\host\matrix_io.cpp .\src\host\validations.cpp -o CUDA_matrix_multiplication -O3 -arch=sm_80 -lineinfo -diag-suppress=611 -I src/host
+nvcc .\src\main.cpp .\src\host\matrix_io.cpp .\src\host\validations.cpp .\src\device\launches.cu -o CUDA_matrix_multiplication -O3 -arch=sm_80 -lineinfo -diag-suppress=611 -I src/host
 pause
