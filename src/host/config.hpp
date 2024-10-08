d5e434f1e360953ed1cc7770a716da28e355259a
vergelli <federico_joelvergelli@hotmail.com>
2024-10-07 21:41:33 -0300

    refactor(bug_4): Simplemente se cambian paths de config

--- src/host/config.hpp
+++ src/host/config.hpp
@@ -9,7 +9,7 @@
  * Esta constante define la ruta del archivo binario donde se almacena la matriz A.
  * Se recomienda actualizarla para que sea configurable de manera dinámica.
  */
-const std::string MATRIX_A_FILE_PATH = "C:\\Users\\feder\\Documents\\Especializacion\\Programming Massively Parallel Processors\\Materias\\1 - Conceptos fundamentales\\Practico\\capitulo_3\\matrix_multiplication\\data\\A_MATRIX.bin";
+const std::string MATRIX_A_FILE_PATH = "Path_to\\A_MATRIX.bin";
 
 /**
  * @brief Ruta del archivo binario que contiene la matriz B.
@@ -17,12 +17,12 @@ const std::string MATRIX_A_FILE_PATH = "C:\\Users\\feder\\Documents\\Especializa
  * Esta constante define la ruta del archivo binario donde se almacena la matriz B.
  * Se recomienda actualizarla para que sea configurable de manera dinámica.
  */
-const std::string MATRIX_B_FILE_PATH = "C:\\Users\\feder\\Documents\\Especializacion\\Programming Massively Parallel Processors\\Materias\\1 - Conceptos fundamentales\\Practico\\capitulo_3\\matrix_multiplication\\data\\B_MATRIX.bin";
+const std::string MATRIX_B_FILE_PATH = "Path_to\\B_MATRIX.bin";
 
 /**
  * @brief Ruta del archivo de metadatos de la matriz resultante C correspondiente a la ejecucion del programa.
  * 
  */
-const std::string METADATA_OUT_PATH = "C:\\Users\\feder\\Documents\\Especializacion\\Programming Massively Parallel Processors\\Materias\\1 - Conceptos fundamentales\\Practico\\capitulo_3\\matrix_multiplication\\data\\";
+const std::string METADATA_OUT_PATH = "Path_to\\data\\";
 
 #endif // CONFIG_HPP
